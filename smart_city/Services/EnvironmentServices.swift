0228cb28b0a2bc7d1253c4e43fe4faac150da10a
KienPham01 <kienluproductions@gmail.com>
2019-09-21 17:07:01 +0700

    handel authentication

---
+++ smart_city/Services/EnvironmentServices.swift
@@ -0,0 +1,14 @@
+//
+//  EnvirontmentServices.swift
+//  smart_city
+//
+//  Created by Apple on 9/21/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+class Environtment: <#super class#> {
+    <#code#>
+}
+//static let instance = MessageService()
---
+++ smart_city/Services/SocketService.swift
@@ -0,0 +1,9 @@
+//
+//  SocketService.swift
+//  smart_city
+//
+//  Created by Apple on 9/21/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
