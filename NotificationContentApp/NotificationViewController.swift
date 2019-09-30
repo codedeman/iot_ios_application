d646370412fea4f99d32a10fdbb21492c45ea17f
KienPham01 <kienluproductions@gmail.com>
2019-09-30 09:24:23 +0700

    aqi cacualation

---
+++ NotificationContentApp/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  NotificationContentApp
+//
+//  Created by Apple on 9/29/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
---
+++ smart_city/Services/UserService.swift
@@ -0,0 +1,9 @@
+//
+//  UserService.swift
+//  smart_city
+//
+//  Created by Apple on 9/30/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
